b94e2b44b048ceca6278119171049de701190e0a
David Hartunian <davidh@knewton.com>
2016-02-22 23:41:16 -0500

    Finished chapter 9

--- cipher.hs
+++ cipher.hs
@@ -1,17 +1,20 @@
 module Cipher where
 import Data.Char
 
-rotate :: Int -> Int -> Int
+genRotate :: Int -> Int -> (Int -> Int -> Int)
 rotateChar :: Int -> Char -> Char
 caesar :: Int -> String -> String
 uncaesar :: Int -> String -> String
 
+genRotate startId numLetters =
+  \n i -> mod (n + (i - startId)) numLetters + startId
 
-rotate n i = mod (n + (i - startId)) numLetters + startId
-  where startId = ord 'a'
-        numLetters = 26
-
-rotateChar n = chr . (rotate n) . ord
+rotateChar n c
+  | elem c ['a'..'z'] = chr . (rotateLower n) . ord $ c
+  | elem c ['A'..'Z'] = chr . (rotateUpper n) . ord $ c
+  | otherwise = c
+    where rotateLower = genRotate (ord 'a') 26
+          rotateUpper = genRotate (ord 'A') 26
 
 caesar = map . rotateChar
 
--- lists.hs
+++ lists.hs
@@ -18,7 +18,6 @@ separateBySpaces s = go s []
 ff :: Num a => a -> a
 ff x = x
 
-
 -- NF   --  [1,2,3,4,5]
 -- WHNF --  1 : 2 : 3 : 4 : _
 -- WHNF --  enumFromTo 1 10
@@ -59,3 +58,56 @@ capitalizeAll (x:xs) = [toUpper x] ++ capitalizeAll xs
 
 getFirstCapital :: String -> Char
 getFirstCapital = toUpper . head
+
+
+-- standard functions
+
+myOr :: [Bool] -> Bool
+myOr [] = False
+myOr (x:xs) = x || myOr xs
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f [] = False
+myAny f (x:xs) = f(x) || myAny f xs
+
+myElem :: Eq a => a -> [a] -> Bool
+--myElem a [] = False
+--myElem a (x:xs) = a == x || myElem a xs
+myElem a = myAny (==a)
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+squish :: [[a]] -> [a]
+squish [] = []
+squish (x:xs) = x ++ squish xs
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f [] = []
+squishMap f (x:xs) = f(x) ++ squishMap f xs
+
+squishAgain :: [[a]] -> [a]
+squishAgain =  squishMap id
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f (x:xs) = go f xs x
+  where go f [] x = x
+        go f (x:xs) x' = case f x' x of
+          GT -> go f xs x'
+          LT -> go f xs x
+          EQ -> go f xs x'
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f (x:xs) = go f xs x
+  where go f [] x = x
+        go f (x:xs) x' = case f x' x of
+          GT -> go f xs x
+          LT -> go f xs x'
+          EQ -> go f xs x
+
+mymax :: (Ord a) => [a] -> a
+mymax = myMaximumBy compare
+
+mymin :: (Ord a) => [a] -> a
+mymin = myMinimumBy compare
